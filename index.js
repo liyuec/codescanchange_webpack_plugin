27ad1d8b3b508af3e9ac1cfc9b4fea30a635e619
CN\Mace Y Li <mace.y.li@cn.pwc.com>
2023-09-12 10:27:04 +0800

    new function: [1]webpack4+ 周期周期切换 [2]webpack5+ 周期切换

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const webpackVersion = require('webpack/package.json').version;
 
 class codescanchange_webpack_plugin{
     /*
@@ -12,43 +13,105 @@ class codescanchange_webpack_plugin{
             throw new Error('must be a Object');
         }
         this.$options = options || {};
-        
+        console.log(`Webpack version: ${webpackVersion}`);
+        debugger;
+        this.$options.webpackVersion = [...webpackVersion][0]
     }
 
     apply(compiler){
         let _compiler = compiler;
 
-        compiler.hooks.afterEmit.tap("codescanchange_webpack_plugin", (compilation) => {
-            debugger;
-            let _compilation = compilation,
-            _assets = _compilation.assets,
-            _buffer = '',
-            _replaceStr = '',
-            codeSpanObj = {},
-            _fileNames = [],
-            _ops = this.$options;
+        if(this.$options.webpackVersion === '4'){
+            compiler.hooks.afterEmit.tap("codescanchange_webpack_plugin", (compilation) => {
+                let _compilation = compilation,
+                _assets = _compilation.assets,
+                _buffer = '',
+                _replaceStr = '',
+                codeSpanObj = {},
+                _fileNames = [],
+                _ops = this.$options,
+                _isEnd = true;
 
-            _fileNames = Object.keys(_compilation.assets);
+                _fileNames = Object.keys(_compilation.assets);
 
-            for(let i = 0;i<_fileNames.length;i++){
-                let _fileName = _fileNames[i],
-                _suffix = _fileName.split('.');
+                for(let i = 0;i<_fileNames.length;i++){
+                    let _fileName = _fileNames[i],
+                    _suffix = _fileName.split('.');
 
-                _suffix = _suffix[_suffix.length - 1];
+                    _suffix = _suffix[_suffix.length - 1];
 
-                if(_fileName.indexOf(_ops.distFileName) > -1 && _suffix !== 'map' && _suffix !== '.css'){
-                    let _filePath = _compilation.assets[_fileName].existsAt;
-                    _buffer = _compiler.inputFileSystem.readFileSync(_filePath);
-                    _replaceStr = _buffer.toString();
-                    //_replaceStr = _replaceStr.replace('this.hoverTimer=setTimeout',`window.macetest='mace_test';this.hoverTimer=setTimeout`)
+                    if(_fileName.indexOf(_ops.distFileName) > -1){
+                        if(_suffix === 'js'){
                     
-                    _replaceStr = _replaceStr.replace(new RegExp(_ops.targetCode, 'g'), _ops.replaceCode);
-                    _compiler.outputFileSystem.writeFile(_filePath,_replaceStr,function(){}); 
+                            let _filePath = _compilation.assets[_fileName].existsAt;
+                            if(!_filePath){
+                                _filePath = _compiler.options.context + '/' + _fileName
+                                _filePath = _filePath.replace(new RegExp('/', 'g'), '\\')
+                            }
+                            _buffer = _compiler.inputFileSystem.readFileSync(_filePath);
+                            _replaceStr = _buffer.toString();
+                            while(_isEnd){
+                                if(_replaceStr.indexOf(_ops.targetCode) < 0){
+                                    _isEnd = false;
+                                }else{
+                                    _replaceStr = _replaceStr.replace(_ops.targetCode, _ops.replaceCode);
+                                }
+                                //_replaceStr = _replaceStr.replace(new RegExp(_ops.targetCode, 'g'), _ops.replaceCode);
+                            }
+                            
+                            _compiler.outputFileSystem.writeFile(_filePath,_replaceStr,function(){}); 
+
+                        }
+                    }
                 }
-            }
+            }); 
+        }else if(this.$options.webpackVersion === '5'){
+            compiler.hooks.assetEmitted.tap("codescanchange_webpack_plugin", (file, { content, source, outputPath, compilation, targetPath }) => {
+            
+                let _compilation = compilation,
+                _buffer = '',
+                _replaceStr = '',
+                codeSpanObj = {},
+                _fileNames = [],
+                _ops = this.$options,
+                _suffix = '',
+                _targetPath = !!targetPath ? targetPath : file,
+                _isEnd = true;
+    
+                //webpack4+,webpack5+ hack
+                _suffix = _targetPath.split('.');
+                _suffix = _suffix[_suffix.length - 1];
+    
+                if(_targetPath.indexOf(_ops.distFileName) > -1){
+                    if(_suffix === 'js'){
+                        let _filePath = _targetPath;
+                        /* if(!_filePath){
+                            _filePath = _compiler.options.context + '/' + _fileName
+                            _filePath = _filePath.replace(new RegExp('/', 'g'), '\\')
+                        } */
+                        _buffer = _compiler.inputFileSystem.readFileSync(_filePath);
+                        _replaceStr = _buffer.toString();
+                        //_replaceStr = _replaceStr.replace(new RegExp(_ops.targetCode, 'g'), _ops.replaceCode);
+                        //_replaceStr = _replaceStr.replace(/_ops.targetCode/g, _ops.replaceCode);
+                        while(_isEnd){
+                            if(_replaceStr.indexOf(_ops.targetCode) < 0){
+                                _isEnd = false;
+                            }else{
+                                _replaceStr = _replaceStr.replace(_ops.targetCode, _ops.replaceCode);
+                            }
+                           
+                        }
+                        //_replaceStr = 'console.log(1234567890);' + _replaceStr;
+                        _compiler.outputFileSystem.writeFile(_filePath,_replaceStr,function(){}); 
+    
+                    }
+                }
+    
+            })
+        }
+          
 
-		});
-
+     
        
            
     }
